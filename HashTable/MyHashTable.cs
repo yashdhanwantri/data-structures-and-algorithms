fd91b14977e4633e1b53c5b45bf5fe82fbac3f80
Yash Dhanwantri <yashdhanwantri@gmail.com>
2020-07-25 17:48:13 +0530

    Collission in HashTable

--- HashTable/MyHashTable.cs
+++ HashTable/MyHashTable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
 using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
@@ -7,42 +8,63 @@ namespace DataStructureAndAlgorithms.HashTable
 {
     class MyHashTable
     {
-        private object[] data;
+        private List<Item>[] data;
         public MyHashTable(int size)
         {
-            data = new object[size];
+            data = new List<Item>[size];
         }
 
+        internal class Item
+        {
+            internal string key;
+            internal int value;
+        }
         private int _generateHash(string key)
         {
             int hash = 0;
             for (int i = 0; i < key.Length; i++)
             {
-                hash = (hash + key[i] * i) % data.Length;
+                hash = (hash + (int)key[i] * i) % data.Length;
             }
             return hash;
         }
 
-        public object set(string key, int value)
+        public List<Item> Set(string key, int value)
         {
             int address = _generateHash(key);
             if (data[address] == null)
             {
-                data[address] = new { key, value };
-                return "Successfully inserted";
-            }
-            else
-                return $"Key {key} already exist.";
-                
-        }
-        public object get(string key)
-        {
-            int address = _generateHash(key);
-            if(data[address]!=null)
-            {
+                data[address] = new List<Item> { new Item { key = key, value = value } };
                 return data[address];
             }
-            return $"Key {key} does not exist";
+            data[address].Add(new Item{ key = key, value = value });
+            return data[address];
+        }
+        public int Get(string key)
+        {
+            int address = _generateHash(key);
+            if (data[address] != null)
+            {
+                for (int i = 0; i < data[address].Count; i++)
+                {
+                    if (data[address][i].key == key)
+                        return data[address][i].value;
+                }
+            }
+                return 99999999;
+        }
+
+        public void Print()
+        {
+            foreach (var item in data)
+            {
+                Console.Write("[ ");
+                foreach (var obj in item)
+                {
+                    Console.Write($"{{ Key: {obj.key}, Value: {obj.value} }}, ");
+                }
+                Console.WriteLine(" ]");
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -39,13 +39,14 @@ namespace DataStructureAndAlgorithms
             //MyArrayList.TestMyArrayList();
 
             //Hash Table Usage
-            MyHashTable hashTable = new MyHashTable(50);
-            hashTable.set("Pineapple", 50);
-            hashTable.set("Apple", 10);
-            Console.WriteLine(hashTable.get("Pineapple"));
-            Console.WriteLine(hashTable.get("Apple"));
-            Console.WriteLine(hashTable.get("Grapes"));
-            
+            MyHashTable hashTable = new MyHashTable(2);
+            hashTable.Set("Pineapple", 50);
+            hashTable.Set("Apple", 10);
+            hashTable.Set("Grapes", 90);
+            Console.WriteLine(hashTable.Get("Pineapple"));
+            Console.WriteLine(hashTable.Get("Apple"));
+            Console.WriteLine(hashTable.Get("Grapes"));
+            hashTable.Print();
         }
     }  
 }
