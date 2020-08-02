a809bec7520ed661361e6dcfeccf6066c39e3383
Yash Dhanwantri <yashdhanwantri@gmail.com>
2020-08-03 03:18:01 +0530

    Binary Search Tree Implementation Started

---
+++ Trees/BinarySearchTree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureAndAlgorithms.Trees
+{
+    sealed class TreeNode
+    {
+        internal int Data { get; set; }
+        internal TreeNode Left { get; set; }
+        internal TreeNode Right { get; set; }
+        internal TreeNode(int Data)
+        {
+            this.Data = Data;
+            this.Left = null;
+            this.Right = null;
+        }
+    }
+    class BinarySearchTree
+    {
+        public TreeNode Root { get; private set; }
+        BinarySearchTree()
+        {
+            Root = null;
+        }
+
+        public void Insert(int Data)
+        {
+            if (Root == null)
+                Root = new TreeNode(Data);
+            else 
+            {
+                if (Root.Data < Data)
+                {
+                    if (Root.Left == null)
+                        Root.Left = new TreeNode(Data);
+                }
+            }
+        }
+        public bool Lookup(int Data)
+        {
+            return false;
+        }
+
+    }
+}
