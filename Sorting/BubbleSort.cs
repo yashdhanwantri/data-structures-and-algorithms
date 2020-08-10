d3c8d5ca9c6c83e9415810a06b0fb21a0d4234eb
Yash Dhanwantri <yashdhanwantri@gmail.com>
2020-08-10 23:07:40 +0530

    Bubble Sort Final

--- Sorting/BubbleSort.cs
+++ Sorting/BubbleSort.cs
@@ -8,15 +8,15 @@ namespace DataStructureAndAlgorithms.Sorting
     {
         public static int[] Sort(int[] input)
         {
-            for(int i= 0;i<input.Length-1; i++)
+            for(int i= 0;i<input.Length; i++)
             {
-                for(int j=i; j<input.Length;j++)
+                for(int j=0; j<input.Length-1;j++)
                 {
-                    if(input[j]<input[i])
+                    if(input[j]>input[j+1])
                     {
-                        var temp = input[i];
-                        input[i] = input[j];
-                        input[j] = temp;
+                        var temp = input[j];
+                        input[j] = input[j+1];
+                        input[j+1] = temp;
                     }
                 }
             }
